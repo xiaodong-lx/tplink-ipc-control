32b820c64cb09a54f6db10e56ca7899851573465
xiaodong-lx <xiaodong-l@qq.com>
2024-06-16 16:00:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# TPLink IPC Control
+
+A simple custom component to control tplink ipc(set lens mask state for now).
+
+## Usage
+
+1. Add an entry in your `configuration.yaml`.
+2. Use service `TPLink IPC Control: Set Lens Mask State` with `ipc url`、`username`、`password`.
+
+## Thanks
+
+`likaci`: [mercury-ipc-control]
+
+[mercury-ipc-control]: https://github.com/likaci/mercury-ipc-control
---
+++ custom_components/tplink_ipc_control/__init__.py
@@ -0,0 +1,32 @@
+from . mipcc_451 import *
+import voluptuous as vol
+
+DOMAIN = "tplink_ipc_control"
+
+ATTR_IPC_URL = "ipc_url"
+DEFAULT_IPC_URL = "http://"
+
+ATTR_USERNAME = "username"
+DEFAULT_USERNAME = "admin"
+
+ATTR_PASSWORD = "password"
+DEFAULT_PASSWORD = "password"
+
+ATTR_SET_LENS_MASK_STATE = "lens_mask_state"
+DEFAULT_SET_LENS_MASK_STATE = ""
+
+def setup(hass, config):
+    def service_set_lens_mask_state(call):
+
+        lens_mask_state = call.data.get(ATTR_SET_LENS_MASK_STATE, DEFAULT_SET_LENS_MASK_STATE)
+        if lens_mask_state == "on" or lens_mask_state == "off":
+            ipc_url = call.data.get(ATTR_IPC_URL, DEFAULT_IPC_URL)
+            username = call.data.get(ATTR_USERNAME, DEFAULT_USERNAME)
+            password = call.data.get(ATTR_PASSWORD, DEFAULT_PASSWORD)
+            data = '{"method":"set","lens_mask":{"lens_mask_info":{"enabled":"' + lens_mask_state + '"}}}'
+            post_data(ipc_url, data, get_stok(ipc_url, username, password))
+        
+    hass.services.register(DOMAIN, "set_lens_mask_state", service_set_lens_mask_state)
+    
+    return True
+  
